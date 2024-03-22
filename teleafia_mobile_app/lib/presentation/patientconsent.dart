b50e3bce2a7c685dddc5e6b8cdc9f9fae362147d
KenKunde <kenkunde94@gmail.com>
2024-03-22 12:26:38 +0300

    Chp Screens

---
+++ teleafia_mobile_app/lib/presentation/patientconsent.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PatientConsent extends StatefulWidget {
+  const PatientConsent({super.key});
+
+  @override
+  State<PatientConsent> createState() => _PatientConsentState();
+}
+
+class _PatientConsentState extends State<PatientConsent> {
+  Color maroon = Color(0xFF982B15);
+  Color background = Color(0xFFFCF4F4);
+  Color darkmaroon = Color(0xFF850808);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(
+        //backgroundColor: background,),
+    body: SafeArea(
+      child: Container(
+        width: MediaQuery.of(context).size.width *1,
+        height: MediaQuery.of(context).size.height *1,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset('assets/Afialogo.jpg',
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(height: 5.0,),
+              Center(
+                child: Container(
+                  child: Text('Patient Consent',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: darkmaroon,
+                  ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 5.0,),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: maroon,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: LinearProgressIndicator(
+                    value: 0.5,
+                    valueColor: AlwaysStoppedAnimation<Color>(maroon),
+                    minHeight: 12.0,
+                  ),
+                ),
+              ),
+            SizedBox(height: 5.0,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'I,',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(5.0, 0.0, 800.0, 0.0),
+                    child: Container(
+                      height: 40.0,
+                      child: TextField(
+                        textAlignVertical: TextAlignVertical.center,
+                        decoration: InputDecoration(
+                          hintText: 'Patient Name',
+                          contentPadding: EdgeInsets.all(5.0),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: maroon),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: maroon),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'hereby consent to the referral to the specialist/facility mentioned above. I understand the purpose of this referral and authorize the sharing of my medical information for the purpose of consultation and treatment',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5.0,),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Text('Patient Signature',
+                           style: TextStyle(
+                             fontWeight: FontWeight.bold,
+                             fontSize: 12.0,
+                             color: maroon,
+                           ),),),
+                      SizedBox(width: 100,),
+                      Expanded(
+                        flex: 1,
+                          child:Text('Nurse Signature',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12.0,
+                            color: maroon,
+                          ),),),
+                      ]),
+                  Container(
+                    height: 40.0,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: TextField(
+                          textAlignVertical: TextAlignVertical.center,
+                          decoration: InputDecoration(
+                              hintText: 'Please sign here',
+                              contentPadding: EdgeInsets.all(5.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: maroon),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: maroon),
+                                borderRadius: BorderRadius.circular(10.0),
+                              )),),
+                        ),
+                        SizedBox(width: 100.0,),
+                        Expanded(
+                          flex: 1,
+                          child: TextField(
+                          textAlignVertical: TextAlignVertical.center,
+                          decoration: InputDecoration(
+                              hintText: 'Please sign here',
+                              contentPadding: EdgeInsets.all(5.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: maroon),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: maroon),
+                                borderRadius: BorderRadius.circular(10.0),
+                              )),),
+                        ),],),
+                  ),],),
+              SizedBox(height: 50.0,),
+              Column(
+                children: [
+                  Text('Please ensure to forward this referral form along'
+                      ' with any accompanying medical records to the reffered'
+                      ' specialist or facility. Thank you for your attention to this matter.',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0,),
+              Container(
+                height: 40.0,
+                child: TextField(
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    hintText: 'Contact information',
+                    contentPadding: EdgeInsets.all(5.0),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: maroon),
+                      borderRadius: BorderRadius.circular(10.0),),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: maroon),
+                      borderRadius: BorderRadius.circular(10.0),),
+                  ),
+                ),
+              ),
+              SizedBox(height: 150.0,),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextButton(onPressed: (){},
+                    style: TextButton.styleFrom(backgroundColor: maroon,
+                      minimumSize: Size(500, 40),
+                    ),
+                    child: Text('SUBMIT',
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white,
+                      ),
+                    ),),),
+              ),
+            ],
+                ),
+        ),
+      ),
+    ),
+    );
+  }
+}
