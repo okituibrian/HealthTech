7ee57c2c056ea470f59c75282dc76991a8d8e107
okituibrian <okituibrian@gmail.com>
2024-05-21 08:47:00 +0300

    updated upload and fetch profile images

--- Teleafia_patient_app/lib/presentation/profile.dart
+++ Teleafia_patient_app/lib/presentation/profile.dart
@@ -1,18 +1,21 @@
-import 'dart:io'; // Import dart:io for File class
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart'; // Add path_provider
 import 'package:teleafia_patient/presentation/medical_history.dart';
 import 'package:teleafia_patient/presentation/my_appointments.dart';
-import 'package:teleafia_patient/presentation/my_health_records.dart';
 import 'package:teleafia_patient/presentation/settings.dart';
 import 'package:teleafia_patient/shared/bottom_nav.dart';
 import 'package:teleafia_patient/shared/header.dart';
 import 'package:teleafia_patient/shared/health_client_functions.dart';
-
 class HealthClientProfile extends StatefulWidget {
-  final XFile? imageFile;
+  final String? avatarSrc;
+  final String? avatarSrcImageUrl;
 
-  const HealthClientProfile({Key? key, this.imageFile}) : super(key: key);
+  const HealthClientProfile({Key? key, this.avatarSrc, this.avatarSrcImageUrl}) : super(key: key);
+
   @override
   _HealthClientProfileState createState() => _HealthClientProfileState();
 }
@@ -20,14 +23,72 @@ class HealthClientProfile extends StatefulWidget {
 class _HealthClientProfileState extends State<HealthClientProfile> {
   Color background = const Color(0xFFFCF4F4);
   Color maroon = const Color(0xFFc00100);
-  Color dark_maroon = const Color(0xFF850808);
-  XFile? _imageFile;
+  Color darkMaroon = const Color(0xFF850808);
+  File? _avatarSrc;
 
-  Future<void> _pickImage(ImageSource source) async {
-    XFile? selected = await ImagePicker().pickImage(source: source);
-    setState(() {
-      _imageFile = selected;
-    });
+  Future<void> _pickFile() async {
+    if (widget.avatarSrcImageUrl != null) {
+      setState(() {
+        _avatarSrc = File(widget.avatarSrcImageUrl!);
+      });
+      return;
+    }
+
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['jpg', 'png', 'pdf'],
+    );
+    if (result != null) {
+      PlatformFile file = result.files.first;
+      File selectedFile = File(file.path!);
+      setState(() {
+        _avatarSrc = selectedFile;
+      });
+      await _uploadImage(selectedFile);
+    }
+  }
+
+
+  Future<void> _uploadImage(File? selected) async {
+    if (selected == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('No image selected')),
+      );
+      return;
+    }
+
+    String apiUrl = 'https://2ac3-102-210-244-74.ngrok-free.app/api/patient/uploadProfileImages/321456789';
+
+    try {
+      var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
+
+      // Adding the image file to the request
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'avatarSrc', // Ensure this matches the backend field name
+          selected.path,
+          filename: path.basename(selected.path), // Get the basename of the file path
+        ),
+      );
+
+      var response = await request.send();
+
+      if (response.statusCode == 200) {
+        print('Succces: ${response.statusCode} => Image uploaded successfully');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Image uploaded successfully')),
+        );
+      } else {
+        print('Error: ${response.statusCode } => ${response.reasonPhrase}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to upload image: ${response.reasonPhrase}')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    }
   }
 
   @override
@@ -41,32 +102,43 @@ class _HealthClientProfileState extends State<HealthClientProfile> {
           children: <Widget>[
             HealthClientHeader(heading: 'Profile'),
             GestureDetector(
-              onTap: () => _pickImage(ImageSource.gallery),
+              onTap: _pickFile,
               child: CircleAvatar(
                 radius: 50,
-                backgroundImage: _imageFile != null
-                    ? FileImage(File(_imageFile!.path))
-                    : AssetImage('assets/default_profile.PNG') as ImageProvider,
+                backgroundImage: _avatarSrc != null
+                    ? FileImage(_avatarSrc!) as ImageProvider<Object>
+                    : widget.avatarSrcImageUrl != null
+                    ? NetworkImage(widget.avatarSrcImageUrl!)
+                    : AssetImage('assets/default_profile.PNG') as ImageProvider<Object>,
               ),
             ),
+
+
+
+
             SizedBox(height: 10),
             MedicalServicesFunctions().customListTile(
               text: 'My History',
               imagePath: 'assets/my_history.PNG',
               onTap: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MedicalHistory(),
-                    ));
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MedicalHistory(),
+                  ),
+                );
               },
             ),
             MedicalServicesFunctions().customListTile(
               text: 'My Appointments',
               imagePath: 'assets/my_appointmentsP.PNG',
               onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MyAppointments()));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyAppointments(),
+                  ),
+                );
               },
             ),
             MedicalServicesFunctions().customListTile(
@@ -80,8 +152,12 @@ class _HealthClientProfileState extends State<HealthClientProfile> {
               text: 'Settings',
               imagePath: 'assets/settings.PNG',
               onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MySettings()));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MySettings(),
+                  ),
+                );
               },
             ),
             MedicalServicesFunctions().customListTile(
@@ -122,6 +198,29 @@ class _HealthClientProfileState extends State<HealthClientProfile> {
   @override
   void initState() {
     super.initState();
-    _imageFile = widget.imageFile;
+    if (widget.avatarSrc != null) {
+      _avatarSrc = File(widget.avatarSrc!);
+    }
+    if (widget.avatarSrcImageUrl != null) {
+      // If avatarSrcImageUrl is not null, convert it to File and assign it to _avatarSrc
+      _loadNetworkImage();
+    }
+  }
+
+  Future<void> _loadNetworkImage() async {
+    final response = await http.get(Uri.parse(widget.avatarSrcImageUrl!));
+    if (response.statusCode == 200) {
+      print('Success: ${response.statusCode}');
+      final bytes = response.bodyBytes;
+      final tempDir = await getTemporaryDirectory();
+      final file = File('${tempDir.path}/avatar_image.jpg');
+      await file.writeAsBytes(bytes);
+      setState(() {
+        _avatarSrc = file;
+      });
+    } else {
+      // Handle error loading network image
+      print('Failed to load network image. Status code: ${response.statusCode}');
+    }
   }
 }
