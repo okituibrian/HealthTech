9206b9c13dfd8460ddaf480865e562f8820dbcc0
okituibrian <okituibrian@gmail.com>
2024-05-21 09:20:51 +0300

    updated the settings page for client profile

--- Teleafia_patient_app/lib/presentation/settings.dart
+++ Teleafia_patient_app/lib/presentation/settings.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:teleafia_patient/presentation/appointment_booking.dart';
-import 'package:teleafia_patient/shared/bottom_nav.dart';
-import 'package:teleafia_patient/shared/header.dart';
+import '../shared/bottom_nav.dart';
+import '../shared/header.dart';
+import 'appointment_booking.dart';
+import 'dashboard.dart';
 
 class MySettings extends StatefulWidget {
   const MySettings({Key? key}) : super(key: key);
@@ -15,7 +16,10 @@ class _MySettingsState extends State<MySettings> {
   final Color maroon = Color(0XFFC00100);
   Color darkMaron = Color(0XFF850808);
   bool pushNotification = false; // Initial toggle state
-  bool darkMode = false; // Initial toggle state
+  bool darkMode = false;
+  bool notification = false; // Initial toggle state
+  TextEditingController _textEditingController = TextEditingController();
+  String? filledText;
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +29,183 @@ class _MySettingsState extends State<MySettings> {
         child: Column(
           children: [
             HealthClientHeader(heading: 'Settings'),
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 8),
+              leading: Text(
+                'Account',
+                style: TextStyle(
+                  color: Color(0XFF850808), // Change color as needed
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15,
+                ),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios_sharp,
+                color: Colors.grey,
+              ),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      backgroundColor: background,
+                      title: Text('Account Details'),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            height: 40,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.red),
+                              borderRadius: BorderRadius.circular(4.0),
+                              color: Colors.white,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 30.0,
+                                  child: TextFormField(
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Full Name',
+                                    ),
+                                    onSaved: (newValue) =>
+                                    filledText = newValue,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 40,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: maroon),
+                              borderRadius: BorderRadius.circular(4.0),
+                              color: Colors.white,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 30.0,
+                                  child: TextFormField(
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Email address',
+                                    ),
+                                    onSaved: (newValue) =>
+                                    filledText = newValue,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            //margin: EdgeInsets.all(5),
+                            height: 40,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.red),
+                              borderRadius: BorderRadius.circular(4.0),
+                              color: Colors.white,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 30.0,
+                                  child: TextFormField(
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Phone Number',
+                                    ),
+                                    onSaved: (newValue) =>
+                                    filledText = newValue,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            // Save the entered details and close the popup overlay
+                            String enteredText = _textEditingController.text;
+                            // Save to the database
+                            print('Entered text: $enteredText');
+                            AccountDetailsDialog.show(context);
+                          },
+                          child: Text(
+                            'Edit',
+                            style: TextStyle(color: maroon),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 8),
+              leading: Text(
+                'Change Password',
+                style: TextStyle(
+                  color: Color(0XFF850808), // Change color as needed
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15,
+                ),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios_sharp,
+                color: Colors.grey,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 8),
+              leading: Text(
+                'Notifications',
+                style: TextStyle(
+                  color: Color(0XFF850808), // Change color as needed
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15,
+                ),
+              ),
+              trailing: IconButton(
+                icon: notification
+                    ? Icon(
+                  Icons.toggle_on_sharp,
+                  color: maroon,
+                )
+                    : Icon(
+                  Icons.toggle_off_sharp,
+                  color: Colors.grey,
+                ),
+                iconSize: 30,
+                color: maroon,
+                onPressed: () {
+                  // Add your desired functionality here
+                  setState(() {
+                    notification = !notification;
+                  });
+                },
+              ),
+            ),
             ListTile(
               contentPadding: EdgeInsets.symmetric(horizontal: 8),
               leading: Text(
@@ -38,14 +219,14 @@ class _MySettingsState extends State<MySettings> {
               trailing: IconButton(
                 icon: pushNotification
                     ? Icon(
-                        Icons.toggle_on_sharp,
-                        color: maroon,
-                      )
+                  Icons.toggle_on_sharp,
+                  color: maroon,
+                )
                     : Icon(
-                        Icons.toggle_off_sharp,
-                        color: Colors.grey,
-                      ),
-                iconSize: 40,
+                  Icons.toggle_off_sharp,
+                  color: Colors.grey,
+                ),
+                iconSize: 30,
                 color: maroon,
                 onPressed: () {
                   // Add your desired functionality here
@@ -68,14 +249,14 @@ class _MySettingsState extends State<MySettings> {
               trailing: IconButton(
                 icon: darkMode
                     ? Icon(
-                        Icons.toggle_on_sharp,
-                        color: maroon,
-                      )
+                  Icons.toggle_on_sharp,
+                  color: maroon,
+                )
                     : Icon(
-                        Icons.toggle_off_sharp,
-                        color: Colors.grey,
-                      ),
-                iconSize: 40,
+                  Icons.toggle_off_sharp,
+                  color: Colors.grey,
+                ),
+                iconSize: 30,
                 onPressed: () {
                   // Add your desired functionality here
                   setState(() {
@@ -94,5 +275,98 @@ class _MySettingsState extends State<MySettings> {
   void bookAppointment() {
     Navigator.push(
         context, MaterialPageRoute(builder: ((context) => BookAppointment())));
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+}
+
+class AccountDetailsDialog extends StatefulWidget {
+  @override
+  _AccountDetailsDialogState createState() => _AccountDetailsDialogState();
+
+  static void show(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AccountDetailsDialog();
+      },
+    );
+  }
+}
+
+class _AccountDetailsDialogState extends State<AccountDetailsDialog> {
+  String filledText = ''; // This is where you can store the entered text
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor:
+      Colors.white, // Change this to your desired background color
+      title: Text('Account Details'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          // Full Name Input
+          buildInputField('Full Name'),
+          SizedBox(height: 10),
+          // Email Address Input
+          buildInputField('Email address'),
+          SizedBox(height: 10),
+          // Phone Number Input
+          buildInputField('Phone Number'),
+          SizedBox(height: 10),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            // Save the entered details and close the popup overlay
+            String enteredText = _textEditingController.text;
+            // Save to the database
+            print('Entered text: $enteredText');
+
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => MySettings()));
+          },
+          child: Text(
+            'Save',
+            style: TextStyle(color: maroon),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildInputField(String hintText) {
+    return Container(
+      height: 40,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.red),
+        borderRadius: BorderRadius.circular(4.0),
+        color: Colors.white,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 30.0,
+            child: TextFormField(
+              controller: TextEditingController(text: filledText),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: hintText,
+              ),
+              onChanged: (newValue) => filledText = newValue,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
